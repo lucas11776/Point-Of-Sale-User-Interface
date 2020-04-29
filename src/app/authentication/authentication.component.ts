f96be3d2662db5385a0c33911d4fb14dc31d7950
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2020-04-29 16:57:12 +0200

    Added authentication module.

---
+++ src/app/authentication/authentication.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pos-authentication',
+  templateUrl: './authentication.component.html',
+  styleUrls: ['./authentication.component.css']
+})
+export class AuthenticationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
